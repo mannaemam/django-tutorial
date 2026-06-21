e4ffa4cbdb17508cbb1b02083bd07a58f859940c
mannaemam <mannaemam@gmail.com>
2026-06-21 23:52:07 +0600

    update polls view

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,22 @@
-from django.http import HttpResponse
-
+from django.http import HttpResponse, Http404
+from .models import Question
+from django.shortcuts import render
 def index(request):
-    return HttpResponse('Hello you are at something.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request,'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except:
+        raise Http404('Question does not exist.')
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = 'You are looking at question %s'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
