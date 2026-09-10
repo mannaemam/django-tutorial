23c1bf04cd9c8df86679435d48348e7a4e1db09a
mannaemam <mannaemam@gmail.com>
2026-09-10 23:30:52 +0600

    merged results

--- polls/views.py
+++ polls/views.py
@@ -14,8 +14,10 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
 def results(request, question_id):
-    return HttpResponse(f'You are looking at results of question{question_id}.')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
