a12019e64de0696fbce5c787e25db09ab715f399
mannaemam <mannaemam@gmail.com>
2026-09-13 21:54:52 +0600

    index view test WIP

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,24 @@
-from django.db.models import QuerySet, F
-from django.http import HttpResponse, HttpResponseRedirect
+from django.db.models import F
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
 
 from polls.models import Question, Choice
-
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[
+            :5
+        ]
 
       
 class DetailView(generic.DetailView):
