8ea99cd23d0206c213b909dff1bddd3ee918b837
mannaemam <mannaemam@gmail.com>
2026-09-22 21:33:38 +0600

    sluggified the urls

--- polls/models.py
+++ polls/models.py
@@ -2,15 +2,32 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 from django.contrib import admin
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=250, unique=True, blank=True)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self) -> str:
         return self.question_text
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.question_text)
+            slug = base_slug
+            counter = 2
+
+            while Question.objects.filter(slug=slug).exists():
+                slug = f'{base_slug}-{counter}'
+                counter += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
 
     @admin.display(
         boolean=True,
